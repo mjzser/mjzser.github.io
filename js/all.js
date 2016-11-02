7549ff7d1ac4f9f30bd505ea39f8c800b5ed39d0
zxy <zhxiyang923@163.com>
2016-11-02 21:13:33 +0800

    '引入animate库'

--- js/all.js
+++ js/all.js
@@ -15,47 +15,10 @@ $(function() {
 $(document).ready(function() {
 	var setHome = $('#setHome');
 	var addToFav = $('#addToFav');
-	//设置为首页
-	// setHome.on('click', function() {
-	// 		SetHomePage();
-	// 	})
-	// 	//加入收藏
-	// addToFav.on('click', function() {
-	// 		AddFavorite();
-	// 	})
-		//设为首页
-		// function SetHomePage() {
-		// 	if (document.all) {
-		// 		document.body.style.behavior = 'url(#default#homepage)';
-		// 		document.body.setHomePage('http://mjzser.com');
-		// 	} else if (window.sidebar) {
-		// 		if (window.netscape) {
-		// 			try {
-		// 				netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-		// 			} catch (e) {
-		// 				alert("该操作被浏览器拒绝，如果想启用该功能，请在地址栏内输入 about:config,然后将项 signed.applets.codebase_principal_support 值该为true");
-		// 			}
-		// 		}
-		// 		var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
-		// 		prefs.setCharPref('browser.startup.homepage', 'http://mjzser.com');
-		// 	}
-		// }
-		// //加入收藏夹
-		// function AddFavorite() {
-		// 	var title = document.title;
-		// 	var url = location.href;
-		// 	if (window.sidebar) {
-		// 		window.sidebar.addPanel(title, url, "");
-		// 	} else if (document.all) {
-		// 		window.external.AddFavorite(url, title);
-		// 	} else {
-		// 		return true;
-		// 	}
-		// }
-		// 
-		// JavaScript Document
 
-
+	$("#weixin_icon").mousemove(function(e){
+		alert('微信二维码');
+	});
 })
 
 //设为首页 < a onclick="SetHome(this,window.location)" > 设为首页 < /a>
