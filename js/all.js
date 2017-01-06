adcd090e466303e3ccad6a2f629778b2d543caa6
zxy <zhxiyang923@163.com>
2017-01-06 08:42:35 +0800

    '注释掉一些冗余的js';

--- js/all.js
+++ js/all.js
@@ -12,43 +12,43 @@ $(function() {
 //         $(this).children('.title').stop().animate({bottom:'-33px'},{queue:false,duration:180});
 //     });
 
-$(document).ready(function() {
-	var setHome = $('#setHome');
-	var addToFav = $('#addToFav');
+// $(document).ready(function() {
+// 	var setHome = $('#setHome');
+// 	var addToFav = $('#addToFav');
 
-	$("#weixin_icon").mousemove(function(e){
-		alert('微信二维码');
-	});
-})
+// 	$("#weixin_icon").mousemove(function(e){
+// 		alert('微信二维码');
+// 	});
+// })
 
 //设为首页 < a onclick="SetHome(this,window.location)" > 设为首页 < /a>
-function setHome(obj, vrl) {
-	try {
-		obj.style.behavior = 'url(#default#homepage)';
-		obj.setHomePage(vrl);
-	} catch (e) {
-		if (window.netscape) {
-			try {
-				netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-			} catch (e) {
-				alert("此操作被浏览器拒绝！\n请在浏览器地址栏输入“about:config”并回车\n然后将 [signed.applets.codebase_principal_support]的值设置为'true',双击即可。");
-			}
-			var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
-			prefs.setCharPref('browser.startup.homepage', vrl);
-		}
-	}
-}
+// function setHome(obj, vrl) {
+// 	try {
+// 		obj.style.behavior = 'url(#default#homepage)';
+// 		obj.setHomePage(vrl);
+// 	} catch (e) {
+// 		if (window.netscape) {
+// 			try {
+// 				netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+// 			} catch (e) {
+// 				alert("此操作被浏览器拒绝！\n请在浏览器地址栏输入“about:config”并回车\n然后将 [signed.applets.codebase_principal_support]的值设置为'true',双击即可。");
+// 			}
+// 			var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
+// 			prefs.setCharPref('browser.startup.homepage', vrl);
+// 		}
+// 	}
+// }
 
 // 加入收藏 < a onclick="AddFavorite(window.location,document.title)" >加入收藏< /a>
 
-function addFavorite(sURL, sTitle) {
-	try {
-		window.external.addFavorite(sURL, sTitle);
-	} catch (e) {
-		try {
-			window.sidebar.addPanel(sTitle, sURL, "");
-		} catch (e) {
-			alert("加入收藏失败，请使用Ctrl+D进行添加");
-		}
-	}
-}
+// function addFavorite(sURL, sTitle) {
+// 	try {
+// 		window.external.addFavorite(sURL, sTitle);
+// 	} catch (e) {
+// 		try {
+// 			window.sidebar.addPanel(sTitle, sURL, "");
+// 		} catch (e) {
+// 			alert("加入收藏失败，请使用Ctrl+D进行添加");
+// 		}
+// 	}
+// }
